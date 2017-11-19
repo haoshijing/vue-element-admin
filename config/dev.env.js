bc0832507c2cfc3024e863c87ab5f540af7841e3
lindsey kong <lindseykong@lindseydeMacBook-Pro.local>
2017-11-19 20:16:42 +0800

    修改本地配置文件

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"development"',
     ENV_CONFIG: '"dev"',
-    BASE_API: '"http://127.0.0.1:8081"',
+    BASE_API: '"http://192.168.3.4:8081"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 }
